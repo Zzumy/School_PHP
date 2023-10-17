5d13a03a6e0a36bccb8e4bad92e3ce1f68f75f5b
Nguyen Duc Duy <nguyen.d.duy@dk.akkszalezi.hu>
2023-10-17 11:26:07 +0200

    teljesen funkcionális lesson/new (felviszi az adatokat)

--- resources/views/lesson/new.blade.php
+++ resources/views/lesson/new.blade.php
@@ -1,6 +1,6 @@
 <form action="/api/lesson" method="post">
     {{csrf_field()}}
-    <input type="text" name="lesson" placeholder="Lesson">
-    <input type="text" name="subject" placeholder="Subject">
+    <input type="text" name="status" placeholder="Status">
+    <input type="text" name="subject_id" placeholder="Subject">
     <input type="submit" value="Ok">
 </form>
