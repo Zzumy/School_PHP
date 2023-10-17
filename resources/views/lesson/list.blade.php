1a62ddd714900781a6951d74ab761bd7e87ffa64
Nguyen Duc Duy <nguyen.d.duy@dk.akkszalezi.hu>
2023-10-17 11:30:43 +0200

    teljesen funkcionális lesson/new (felviszi az adatokat) rendesen megjeleníti az adatokat

--- resources/views/lesson/list.blade.php
+++ resources/views/lesson/list.blade.php
@@ -2,6 +2,7 @@
     <form action="/api/lesson/{{$lesson->lesson_id}}" method="post">
         {{csrf_field()}}
         {{method_field('GET')}}
-        <input type="submit" value="{{$lesson->subject}}">
+        <input type="submit" value="{{$lesson->status}}">
+        <input type="submit" value="{{$lesson->subject_id}}">
     </form>
 @endforeach
