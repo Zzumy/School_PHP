1f96b07b809578839975a47788ab82317366da6b
Nguyen Duc Duy <nguyen.d.duy@dk.akkszalezi.hu>
2023-10-17 11:46:10 +0200

    dropdown select option lesson id-hoz

--- resources/views/lesson/new.blade.php
+++ resources/views/lesson/new.blade.php
@@ -1,6 +1,9 @@
 <form action="/api/lesson" method="post">
     {{csrf_field()}}
     <input type="text" name="status" placeholder="Status">
-    <input type="text" name="subject_id" placeholder="Subject">
+    <select name="subject_id">
+        <option value="1">1</option>
+        <option value="2">2</option>
+    </select>
     <input type="submit" value="Ok">
 </form>
